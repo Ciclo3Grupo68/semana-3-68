64bb9251e42676745b8677f1492d5ac93535574b
Diana Maria Ordoñea Almonacid <mein_herz-brennt@hotmail.com>
2020-12-14 08:46:34 +0100

    Backend pasando todas las pruebas

--- server.js
+++ server.js
@@ -3,30 +3,45 @@ debe importar como se observa en la siguiente linea, con el nombre del archivo j
 que contiene la logica */
 //const controller = require('./controller/nombredelcontrollador.js');
 const express = require('express');
+const morgan = require('morgan');
+const apiRouter = require('./routes/api/index.js');
 const db = require('./models');
-const app = express()
+const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+const controller = require('./controllers/UserController.js');
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     next();
 });
 
 
-app.use(bodyParser.json())
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API ENDPOINTS
-/*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
-'/api/auth/signin'
-*/
-app.get('/', function(req, res) {
+app.use('/api', apiRouter);
+
+app.set('PORT', process.env.PORT || 3000);
+
+app.get('/', function(req,res){
     console.log("Estructura base del proyecto backend");
     res.send("Estructura base del proyecto backend");
 });
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
-})
+
+app.get('/api/users', (req, res) => {
+    db.user.findAll().then(users => res.json(users))
+});
+
+app.post('/api/auth/signin', controller.signin);
+
+
+app.listen(app.get('PORT'), () => {
+    console.log(`Running on http://localhost:${app.get('PORT')}`)
+});
 
 module.exports = app;
